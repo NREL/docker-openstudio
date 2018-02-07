b144d44065aee94da26f562492f8ce7da436fdcb
Nicholas Long <nicholas.long@nrel.gov>
2018-02-06 21:26:30 -0700

    fix imagetag

--- deploy_docker.sh
+++ deploy_docker.sh
@@ -5,9 +5,10 @@ if [ "${TRAVIS_BRANCH}" == "develop" ]; then
     IMAGETAG=develop
 elif [ "${TRAVIS_BRANCH}" == "master" ]; then
     # Retrieve the version number from package.json
-    IMAGETAG=$( docker run -it openstudio:latest ruby -r openstudio -e "puts OpenStudio.openStudioVersion" | tr '\n' '' )
+    IMAGETAG=$( docker run -it openstudio:latest ruby -r openstudio -e "puts OpenStudio.openStudioVersion" )
     OUT=$?
     if [ $OUT -eq 0 ]; then
+        IMAGETAG=$( echo $IMAGETAG | tr -d '\r' )
         echo "Found OpenStudio Version: $IMAGETAG"
     else
         echo "ERROR Trying to find OpenStudio Version"
@@ -19,13 +20,9 @@ if [ "${IMAGETAG}" != "skip" ] && [ "${TRAVIS_PULL_REQUEST}" == "false" ]; then
     echo "Tagging image as $IMAGETAG"
 
     docker login -u $DOCKER_USER -p $DOCKER_PASS
-    docker build -f Dockerfile -t nrel/openstudio:$IMAGETAG .
-    docker tag nrel/openstudio nrel/openstudio:$IMAGETAG
+    docker build -f Dockerfile -t nrel/openstudio:$IMAGETAG -t nrel/openstudio:latest .
     docker push nrel/openstudio:$IMAGETAG
-
-    # also push to latest - since there really isn't a develop on this repo
-    docker tag nrel/openstudio nrel/openstudio:latest
     docker push nrel/openstudio:latest
 else
-    echo "Not on a deployable branch, this is a pull request"
+    echo "Not on a deployable branch, this is a pull request or has been explicity skipped"
 fi
