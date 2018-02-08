a7a79dee54e521b61a030467cff9541e82a96aa6
Julien Marrec <julien.marrec@gmail.com>
2018-02-08 11:28:54 +0100

    Update deploy script to check if base image exists and if older than 1 month
    Note, you'll need to adapt it for Travis itself

--- deploy_docker.sh
+++ deploy_docker.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+base_image_name=nrel/openstudio:base
+
 IMAGETAG=skip
 if [ "${TRAVIS_BRANCH}" == "develop" ]; then
     IMAGETAG=develop
@@ -19,7 +21,43 @@ fi
 if [ "${IMAGETAG}" != "skip" ] && [ "${TRAVIS_PULL_REQUEST}" == "false" ]; then
     echo "Tagging image as $IMAGETAG"
 
+    # Login to docker
     docker login -u $DOCKER_USER -p $DOCKER_PASS
+
+    # If the base image doesn't already exists, build it
+    if [ -z $(docker images -q $base_image_name) ]; then
+      echo "Building $base_image_name (not found)"
+      docker build -f Dockerfile-Base -t $base_image_name .
+      # Not sure how travis works (can it store the local base image?)
+      # Otherwise push it
+      docker push $base_image_name
+    else
+      # Otherwise, Check whether the base image is older than a month
+      base_image_created_date=$(docker inspect -f '{{ .Created }}' $base_image_name)
+      date_sec_base_image=$(date -d $base_image_created_date +%s)
+      date_sec_now=$(date +%s)
+      older_than_one_month=$(( ($date_sec_now - $date_sec_base_image) > 60*24*30))
+
+      if [ $older_than_one_month ]; then
+        # If older than a month, ask whether the user wants to rebuild
+        # For TRAVIS I guess you can just delete that and force rebuild
+        echo "The base image $base_image_name is older than one month, rebuild? [Y/n]"
+        read -n 1 -r
+        echo    # (optional) move to a new line
+        # Default is yes, so anything else than 'n' will trigger rebuild
+        if [[ ! $REPLY =~ ^[Nn]$ ]]
+        then
+            echo -e "* Rebuilding the base image $base_image_name from Dockerfile-Base"
+            docker rmi $base_image_name
+            docker build -f Dockerfile-Base -t $base_image_name .
+            # Push?
+            docker push $base_image_name
+        fi
+      else
+        echo "Found the base image $base_image_name which is newer than one month"
+      fi
+    fi
+
     docker build -f Dockerfile -t nrel/openstudio:$IMAGETAG -t nrel/openstudio:latest .
     docker push nrel/openstudio:$IMAGETAG
     docker push nrel/openstudio:latest
