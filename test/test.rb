7ff427629a067ecd41a63285a309e8c79a15ac16
Nicholas Long <nicholas.long@nrel.gov>
2018-05-08 10:27:16 -0600

    remove old test file. remove test files before running new openstudio simulation

--- test/test.rb
+++ test/test.rb
@@ -29,11 +29,12 @@ puts "OpenStudio Version: #{OpenStudio.openStudioLongVersion}"
 puts OpenStudio::Model.exampleModel.to_s
 
 # Grab the test files that are shipped with OpenStudio and put into a folder and run
+FileUtils.rm_rf 'test/compact_osw'
 if OpenStudio.openStudioVersion.to_version > '2.5.1'.to_version
-  FileUtils.cp_r "/usr/local/openstudio-#{OpenStudio.openStudioVersion}/Examples/compact_osw", 'test/.', noop: true
+  FileUtils.cp_r "/usr/local/openstudio-#{OpenStudio.openStudioVersion}/Examples/compact_osw", 'test/.'
   `/usr/local/bin/openstudio run -w /var/simdata/openstudio/test/compact_osw/compact.osw`
 else 
-  FileUtils.cp_r "/usr/Examples/compact_osw", 'test/.', noop: true
+  FileUtils.cp_r "/usr/Examples/compact_osw", 'test/.'
   `/usr/bin/openstudio run -w /var/simdata/openstudio/test/compact_osw/compact.osw`
 end
 raise "Simulation did not run" unless File.exist?('test/compact_osw/run/finished.job')
