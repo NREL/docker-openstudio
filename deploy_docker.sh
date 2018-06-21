d2a42ed2b32adbb465de469f4ec0edc607e51110
Nicholas Long <nicholas.long@nrel.gov>
2018-06-20 23:36:25 -0600

    error in deploy

--- deploy_docker.sh
+++ deploy_docker.sh
@@ -19,15 +19,17 @@ fi
 if [ "${IMAGETAG}" != "skip" ] && [ "${TRAVIS_PULL_REQUEST}" == "false" ]; then
     echo "Tagging image as $IMAGETAG"
 
-    docker login -u $DOCKER_USER -p $DOCKER_PASS
-    docker tag openstudio:latest nrel/openstudio:$IMAGETAG
-    docker tag openstudio:latest nrel/openstudio:latest
-    docker push nrel/openstudio:$IMAGETAG
+    docker login -u $DOCKER_USER -p $DOCKER_PASS; (( exit_status = exit_status || $? ))
+    docker tag openstudio:latest nrel/openstudio:$IMAGETAG; (( exit_status = exit_status || $? ))
+    docker tag openstudio:latest nrel/openstudio:latest; (( exit_status = exit_status || $? ))
+    docker push nrel/openstudio:$IMAGETAG; (( exit_status = exit_status || $? ))
 
     if [ "${TRAVIS_BRANCH}" == "master" ]; then
 	# Deploy master as the latest.
-        docker push nrel/openstudio:latest
+        docker push nrel/openstudio:latest; (( exit_status = exit_status || $? ))
     fi
+
+    exit exit_status
 else
     echo "Not on a deployable branch, this is a pull request or has been explicity skipped"
 fi
