70ae23cdc10d592a66d3225e67fefc645a460cba
Noel Merket <noel.merket@nrel.gov>
2018-06-26 09:20:49 -0600

    making built image smaller

--- singularity/build_image.sh
+++ singularity/build_image.sh
@@ -2,8 +2,9 @@ if [ ! "$(docker ps -q -f name=for_export)" ]; then docker rm for_export; else e
 # start an instance of the container for export
 docker run --name for_export docker-openstudio /bin/true
 if [ -f docker-openstudio.simg ]; then rm -f docker-openstudio.simg; else echo "File does not exist"; fi
-singularity image.create -s 2000 docker-openstudio.simg
-docker export for_export | singularity image.import docker-openstudio.simg
+docker export for_export > /tmp/docker-export.tar
+singularity build docker-openstudio.simg /tmp/docker-export.tar
+rm /tmp/docker-export.tar
 
 # test singularity
 #singularity shell -B $(pwd):/singtest docker-openstudio.simg
