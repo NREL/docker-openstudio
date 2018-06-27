538bc1c7f56ccf453f29f3006d18c1fbcabf2ffe
Nicholas Long <nicholas.long@nrel.gov>
2018-06-26 19:38:29 -0600

    registry network

--- singularity/deploy_singularity.sh
+++ singularity/deploy_singularity.sh
@@ -11,6 +11,8 @@ docker run -d -p 5000:5000 --restart=always --name registry registry:2
 docker tag docker-openstudio localhost:5000/docker-openstudio
 docker push localhost:5000/docker-openstudio
 
+docker ps
+
 # Launch the singularity container
 docker run -it --rm --privileged --network=container:registry -v $(pwd):/root/build -v /var/run/docker.sock:/var/run/docker.sock singularity /root/build/singularity/build_singularity.sh
 
