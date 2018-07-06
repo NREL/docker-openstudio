f8e9d4970c9ad52d3ee0d5f367d5312ce715641d
Nicholas Long <nicholas.long@nrel.gov>
2018-07-06 15:45:52 -0600

    git gems do not work

--- test/test_gemfile_git.rb
+++ test/test_gemfile_git.rb
@@ -4,13 +4,12 @@ def local_gems
    Gem::Specification.sort_by{ |g| [g.name.downcase, g.version] }.group_by{ |g| g.name }
 end
 
+# list installed gems
 puts local_gems.map{ |name, specs| 
-  [ 
-    name,
-    specs.map{ |spec| spec.version.to_s }.join(',')
-  ].join(' ') 
+  [name, specs.map{ |spec| spec.version.to_s }.join(',')].join(' ') 
 }
 
+# test a github checkout gem
 require 'openstudio'
 require 'openstudio-standards'
 puts OpenstudioStandards::VERSION
