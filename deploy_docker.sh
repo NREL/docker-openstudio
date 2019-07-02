97514cdef3a9b263e4c0ba9d29624de84bacfd05
Petersen, Anya <Anya.Petersen@nrel.gov>
2019-07-02 11:24:18 -0600

    add publishing for develop3 and remove -cli image stuff since develop3 is cli only

--- deploy_docker.sh
+++ deploy_docker.sh
@@ -4,6 +4,8 @@ echo "image would be tagged as $IMAGETAG if this were master branch"
 IMAGETAG=skip
 if [ "${TRAVIS_BRANCH}" == "develop" ]; then
     IMAGETAG=develop
+elif [ "${TRAVIS_BRANCH}" == "develop3" ]; then
+    IMAGETAG=develop3
 elif [ "${TRAVIS_BRANCH}" == "master" ]; then
     IMAGETAG=${OPENSTUDIO_VERSION}${OPENSTUDIO_VERSION_EXT}
 fi
@@ -14,20 +16,14 @@ if [ "${IMAGETAG}" != "skip" ] && [ "${TRAVIS_PULL_REQUEST}" == "false" ]; then
     echo "$DOCKER_PASS" | docker login -u "$DOCKER_USER" --password-stdin
     docker tag openstudio:latest nrel/openstudio:$IMAGETAG; (( exit_status = exit_status || $? ))
     docker tag openstudio:latest nrel/openstudio:latest; (( exit_status = exit_status || $? ))
-    # docker tag openstudio-cli:latest nrel/openstudio-cli:$IMAGETAG; (( exit_status = exit_status || $? ))
-    # docker tag openstudio-cli:latest nrel/openstudio-cli:latest; (( exit_status = exit_status || $? ))
     docker push nrel/openstudio:$IMAGETAG; (( exit_status = exit_status || $? ))
-    # docker push nrel/openstudio-cli:$IMAGETAG; (( exit_status = exit_status || $? ))
 
     if [ "${TRAVIS_BRANCH}" == "master" ]; then
 	# Deploy master as the latest.
         docker push nrel/openstudio:latest; (( exit_status = exit_status || $? ))
-        # docker push nrel/openstudio-cli:latest; (( exit_status = exit_status || $? ))
     fi
 
     exit $exit_status
 else
     echo "Not on a deployable branch, this is a pull request or has been explicity skipped"
 fi
-
-# Deploy the singularity image
