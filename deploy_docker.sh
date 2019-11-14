e7423b3ed84787a96cd01acbddfdc504713b9b74
tijcolem <tjc.coleman@gmail.com>
2019-11-14 15:02:16 -0700

    updating image tag for 3.0-pre1-rc1

--- deploy_docker.sh
+++ deploy_docker.sh
@@ -5,7 +5,7 @@ IMAGETAG=skip
 if [ "${TRAVIS_BRANCH}" == "develop" ]; then
     IMAGETAG=develop
 elif [ "${TRAVIS_BRANCH}" == "develop3" ]; then
-    IMAGETAG=3-prerelease-rc2-2.8.1
+    IMAGETAG=3.0.0-pre1-rc1
 elif [ "${TRAVIS_BRANCH}" == "master" ]; then
     IMAGETAG=${OPENSTUDIO_VERSION}${OPENSTUDIO_VERSION_EXT}
 fi
