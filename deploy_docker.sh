13d3158aad542ddc6bfcb03603e7619fb3eb5909
Brian Ball <brian.ball@nrel.gov>
2025-05-24 08:47:03 -0500

    custom build 3.10-alpha

--- deploy_docker.sh
+++ deploy_docker.sh
@@ -12,7 +12,7 @@ elif [ "${GITHUB_REF}" == "refs/heads/master" ]; then
     # Retrieve the version number from rails
     IMAGETAG=${OPENSTUDIO_VERSION}${OPENSTUDIO_VERSION_EXT}
 # Uncomment and set branch name for custom builds.
-elif [ "${GITHUB_REF}" == "refs/heads/custom_branch_name" ]; then
+elif [ "${GITHUB_REF}" == "refs/heads/3.10-alpha" ]; then
     IMAGETAG="experimental"
 elif [ "${DOCKER_MANUAL_IMAGE_TAG}" == "develop" ]; then
     IMAGETAG="develop"
